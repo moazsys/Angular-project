c09b85b6bf7c1cbc549e6aacefce4b1d88410f6e
moazsys <m.moaznaeem@systemsltd.com>
2024-10-08 15:00:53 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,14 @@
-
-# Variables
-NEXUS_URL="http://4.216.187.218:8081/repository/angular/"  # Corrected URL format
+#!/bin/bash
+NEXUS_URL="http://4.216.187.218:8081"
 REPOSITORY="angular"
 VERSION="1.0.0"
-FILE_PATH="/var/jenkins_home/workspace/publish/publish.zip"           # Added leading '/'
+FILE_PATH="publish.zip" # Ensure this matches your file name
 USERNAME="admin"
 PASSWORD="Moaz@2003"
 
 # Publish to Nexus
 curl -v -u "$USERNAME:$PASSWORD" --upload-file "$FILE_PATH" \
-    "$NEXUS_URL/repository/$REPOSITORY/$VERSION/publish.zip"  # Include version in the path
+    "$NEXUS_URL/repository/$REPOSITORY/$VERSION/$FILE_PATH"
 
 if [ $? -eq 0 ]; then
     echo "File published successfully to Nexus!"
