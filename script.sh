c53757110bf3f7cac94f190e132073097d832d4c
moazsys <m.moaznaeem@systemsltd.com>
2024-10-08 15:46:15 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,15 @@
-#!/bin/bash
-
 NEXUS_URL="http://4.216.187.218:8081"
 REPOSITORY="angular"
 VERSION="1.0.0"
-DIRECTORY_PATH="nexus"  # Adjust to your target directory
+FILE_PATH="app.tar" 
 USERNAME="admin"
 PASSWORD="Moaz@2003"
 
-# Create a tarball of the directory
-TARBALL_NAME="${DIRECTORY_PATH}.tar.gz"
-tar -czf "$TARBALL_NAME" "$DIRECTORY_PATH"
-
-# Check if the tarball was created successfully and is not empty
-if [ ! -f "$TARBALL_NAME" ] || [ ! -s "$TARBALL_NAME" ]; then
-    echo "Failed to create a valid tarball!"
-    exit 1
-fi
-
-# Publish to Nexus
-curl -v -u "$USERNAME:$PASSWORD" --upload-file "$TARBALL_NAME" \
-    "$NEXUS_URL/repository/$REPOSITORY/$VERSION/$TARBALL_NAME"
+curl -v -u "$USERNAME:$PASSWORD" --upload-file "$FILE_PATH" \
+    "$NEXUS_URL/repository/$REPOSITORY/$VERSION/$FILE_PATH"
 
 if [ $? -eq 0 ]; then
-    echo "Directory published successfully to Nexus!"
+    echo "File published successfully to Nexus!"
 else
-    echo "Failed to publish directory to Nexus."
+    echo "Failed to publish file to Nexus."
 fi
