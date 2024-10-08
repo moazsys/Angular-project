6ebb7223c06e23f3a3321737b735c096c36cfd8b
moazsys <m.moaznaeem@systemsltd.com>
2024-10-08 14:47:43 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
 
 # Variables
-NEXUS_URL="http://4.216.187.218:8081/repository/angular/"
+NEXUS_URL="http://4.216.187.218:8081"  # Corrected URL format
+REPOSITORY="angular"
+VERSION="1.0.0"
+FILE_PATH="/var/jenkins_home/workspace/publish/publish.zip"           # Added leading '/'
 USERNAME="admin"
 PASSWORD="Moaz@2003"
-APP_DIR="$(pwd)"  # Assuming the script is run from the app directory
-
-# Create .npmrc file for authentication
-echo "//4.216.187.218:8081/repository/angular/:username=${USERNAME}" > ${APP_DIR}/.npmrc
-echo "//4.216.187.218:8081/repository/angular/:_password=$(echo -n ${PASSWORD} | base64)" >> ${APP_DIR}/.npmrc
-
-# Ensure package.json exists
-if [ ! -f "${APP_DIR}/package.json" ]; then
-    echo "package.json not found. Make sure you're in the correct directory."
-    exit 1
-fi
 
 # Publish to Nexus
-npm publish --registry $NEXUS_URL
+curl -v -u "$USERNAME:$PASSWORD" --upload-file "$FILE_PATH" \
+    "$NEXUS_URL/repository/$REPOSITORY/$VERSION/publish.zip"  # Include version in the path
 
-# Check for success
-if [[ $? -eq 0 ]]; then
-    echo "Application published successfully to Nexus!"
+if [ $? -eq 0 ]; then
+    echo "File published successfully to Nexus!"
 else
-    echo "Failed to publish application to Nexus."
-    exit 1
+    echo "Failed to publish file to Nexus."
 fi
