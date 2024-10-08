fdc4d9047f11d1d4752405c486a146005b80fd3a
moazsys <m.moaznaeem@systemsltd.com>
2024-10-08 17:18:24 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 NEXUS_URL="http://4.216.187.218:8081/repository/angular/"
 USERNAME="admin"
 PASSWORD="Moaz@2003"
-APP_DIR="/var/jenkins_home/workspace/jenkins_nexus/"
-APP_NAME="my-angular-app"  
+APP_DIR="/var/jenkins_home/workspace/publish/"
+APP_NAME="my-python-app"  
 VERSION="1.1.0"           
 
 # Create .npmrc file for authentication
@@ -15,12 +15,12 @@ cat <<EOF > ${APP_DIR}/package.json
 {
   "name": "${APP_NAME}",
   "version": "${VERSION}",
-  "main": "app.tar",
+  "main": "publish.tar",
   "files": [
-    "app.tar"
+    "publish.tar"
   ],
   "scripts": {
-    "start": "app.tar"
+    "start": "publish.tar"
   },
   "engines": {
     "node": ">=14.0.0"
