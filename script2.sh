af5ad79d7d28bf390e89789fcb890d624aa7c0c7
moazsys <m.moaznaeem@systemsltd.com>
2024-10-09 11:44:32 +0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -3,21 +3,17 @@ NEXUS_URL="http://4.216.187.218:8081/repository/angular/"
 USERNAME="admin"
 PASSWORD="Moaz@2003"
 APP_DIR="/var/jenkins_home/workspace/nexus11"
-ZIP_FILE="angular.zip"  # Zip file name
-APP_NAME="Angular"       # Change this to your desired app name
-VERSION="1.0.0"          # Version of your package
+ZIP_FILE="angular.zip" 
+APP_NAME="Angular"       
+VERSION="1.0.0"         
 
-# Change to the application directory
 cd "${APP_DIR}" || { echo "Application directory not found"; exit 1; }
 
-# Create a zip of the application directory
 zip -r "${ZIP_FILE}" ./*
 
-# Create .npmrc file for authentication
 echo "//4.216.187.218:8081/repository/angular/:username=${USERNAME}" > ${APP_DIR}/.npmrc
 echo "//4.216.187.218:8081/repository/angular/:_password=$(echo -n ${PASSWORD} | base64)" >> ${APP_DIR}/.npmrc
 
-# Create a package.json file
 cat <<EOF > ${APP_DIR}/package.json
 {
   "name": "${APP_NAME}",
