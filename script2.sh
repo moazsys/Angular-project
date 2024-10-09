f36074c30fa21f1e6830e2bc43db99e3a1ed3b12
moazsys <m.moaznaeem@systemsltd.com>
2024-10-09 11:37:55 +0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,26 +1,17 @@
-#!/bin/bash
 
-# Variables
-GIT_REPO="https://github.com/your_username/nexus11.git"  # Replace with your GitHub repo
 NEXUS_URL="http://4.216.187.218:8081/repository/angular/"
 USERNAME="admin"
 PASSWORD="Moaz@2003"
 APP_DIR="/var/jenkins_home/workspace/nexus11"
-APP_NAME="nexus11-app"  # Change this to your desired app name
-VERSION="1.0.0"         # Version of your package
+ZIP_FILE="angular.zip"  # Zip file name
+APP_NAME="Angular"       # Change this to your desired app name
+VERSION="1.0.0"          # Version of your package
 
-# Clone the GitHub repository
-git clone $GIT_REPO $APP_DIR
+# Change to the application directory
+cd "${APP_DIR}" || { echo "Application directory not found"; exit 1; }
 
-# Check if clone was successful
-if [[ $? -ne 0 ]]; then
-    echo "Failed to clone repository."
-    exit 1
-fi
-
-# Create a zip file of the cloned directory
-cd /var/jenkins_home/workspace || { echo "Workspace directory not found"; exit 1; }
-zip -r nexus11.zip nexus11
+# Create a zip of the application directory
+zip -r "${ZIP_FILE}" ./*
 
 # Create .npmrc file for authentication
 echo "//4.216.187.218:8081/repository/angular/:username=${USERNAME}" > ${APP_DIR}/.npmrc
@@ -31,21 +22,18 @@ cat <<EOF > ${APP_DIR}/package.json
 {
   "name": "${APP_NAME}",
   "version": "${VERSION}",
-  "main": "nexus11.zip",
+  "main": "${ZIP_FILE}",
   "files": [
-    "nexus11.zip"
+    "${ZIP_FILE}"
   ],
   "scripts": {
-    "start": "unzip nexus11.zip && cd nexus11 && npm start"
+    "start": "unzip ${ZIP_FILE}"
   },
   "engines": {
     "node": ">=14.0.0"
   }
 }
 EOF
-
-# Change to the application directory
-cd "${APP_DIR}" || { echo "Application directory not found"; exit 1; }
 
 # Publish to Nexus
 npm publish --registry $NEXUS_URL
